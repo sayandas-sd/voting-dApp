b09eeaf111a0ebaa7fc751df5b9be32099ce3767
sayandas-sd <sayandasmac@gmail.com>
2024-10-29 23:45:47 +0530

    added voting program spec.ts

--- anchor/tests/dvoteapp.spec.ts
+++ anchor/tests/dvoteapp.spec.ts
@@ -6,12 +6,27 @@ import { BankrunProvider, startAnchor } from "anchor-bankrun";
 
 const IDL = require('../target/idl/dvoteapp.json');
 
+const votingAddress = new PublicKey("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 
 describe('Dvoteapp', () => {
  
  
   it('Initialize Poll', async () => {
-    
+    const context =  await startAnchor("", [{name: "voting", programId: votingAddress}], []);
+    const provider = new BankrunProvider(context);
+
+    const votingProgram = new Program<Dvoteapp>(
+      IDL,
+      provider,
+    );
+
+
+    await votingProgram.methods.initializePoll(
+      new anchor.BN(1),
+      "what is your peanut butter?",
+      new anchor.BN(0), 
+      new anchor.BN(1830217343),
+    ).rpc();
 
 
   })
